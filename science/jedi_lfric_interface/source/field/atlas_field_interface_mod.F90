a83dac52a60b00bd7afa77d1cf84f37f96d2f442
rickywong <enquiries@metoffice.gov.uk>
2024-06-13 12:01:39 +0000

    ticket:87 : stevensandbach : Add Adjoint copy operations in the Atlas-LFRic field interface
    git-svn-id: https://metomi/svn/lfric_apps.xm/main/trunk@1910 3896877f-8c05-4358-bc88-7947bb4308c7

--- science/jedi_lfric_interface/source/field/atlas_field_interface_mod.F90
+++ science/jedi_lfric_interface/source/field/atlas_field_interface_mod.F90
@@ -4,14 +4,37 @@
 ! under which the code may be used.
 !-----------------------------------------------------------------------------
 !
-!> @brief A module providing an Atlas field class.
+!> @brief A module providing an Atlas-LFRic interface field class.
 !>
 !> @details The atlas field interface can hold a 64-bit real pointer to data
-!> that is stored externaly. The field can be copied to and from LFRic fields
+!> that is stored externally. The field can be copied to and from LFRic fields
 !> stored in the lowest order W3 and Wtheta function-space including 2D fields.
 !> Methods are provided to perform the copies that rely on access to the field
-!> proxy. Only owned LFRic points are updated.
-!
+!> proxy. Only owned LFRic and Atlas points are updated.
+!>
+!> Adjoint of the data copy methods are also included. Noting that "=" is
+!> ambiguous because it may represent a move or copy, it is assumed that a copy
+!> is intended and add a rule: the adjoint of "falling out of scope" is
+!> initialisation to zero. In the below adjoint code, where "=" is encountered
+!> in the forward code:
+!>
+!> b=a
+!>
+!> we apply "+=" followed by initialisation to zero in the adjoint:
+!>
+!> a_hat=a_hat+b_hat
+!> b_hat=0
+!>
+!> This results in non intuitive naming of functions. The adjoint of copying
+!> "from" LFRic is an update "to" the LFRic field. The adjoint code below is
+!> a standard line by line adjoint where the outlined rules are adopted.
+!>
+!> The convention noted above states that we need to include an additional
+!> rule: The adjoint of "falling out of scope" is "initialisation to zero".
+!> This means that in addition to the adjoint data copies, we also need to
+!> include a method to zero the fields. To enable that, two further methods
+!> (zero_lfric and zero_atlas) are included.
+!>
 module atlas_field_interface_mod
 
   use, intrinsic :: iso_fortran_env, only : real64
@@ -71,6 +94,18 @@ contains
   !> Copy atlas field to the LFRic field
   procedure, public :: copy_to_lfric
 
+  !> Adjoint of: (Copy atlas field from the LFRic field)
+  procedure, public :: copy_from_lfric_ad
+
+  !> Adjoint of: (Copy atlas field to the LFRic field)
+  procedure, public :: copy_to_lfric_ad
+
+  !> Zero the LFRic field
+  procedure, public :: zero_lfric
+
+  !> Zero the Atlas field
+  procedure, public :: zero_atlas
+
   !> Get the LFRic field name
   procedure, public :: get_lfric_name
 
@@ -87,15 +122,17 @@ end type atlas_field_interface_type
 !-------------------------------------------------------------------------------
 contains
 
-!> @param [in] atlas_data_ptr pointer to the atlas data
-!> @param [in] map_horizontal_ptr pointer to the horizontal map
-!> @param [in] lfric_field_ptr the LFRic field that atlas_field_interface will
-!>             copy to and from
-!> @param [in] surface_level_type enumerator defining how the lowest level
-!>             is treated.
-!> @param [in] atlas_name Optional name of the atlas field
-!> @param [in] fill_direction_up Optional logical set false if the atlas
-!>               field data is orientated from top-bottom
+!> @brief atlas_field_interface_type initialiser
+!>
+!> @param [in] atlas_data_ptr     Pointer to the atlas data
+!> @param [in] map_horizontal_ptr Pointer to the horizontal map
+!> @param [in] lfric_field_ptr    The LFRic field that atlas_field_interface
+!>                                will copy to and from
+!> @param [in] surface_level_type Enumerator defining how the lowest level
+!>                                is treated.
+!> @param [in] atlas_name         Optional name of the atlas field
+!> @param [in] fill_direction_up  Optional logical set false if the atlas field
+!>                                data is orientated from top-bottom
 subroutine field_initialiser( self, atlas_data_ptr, map_horizontal_ptr, &
                               lfric_field_ptr, surface_level_type, &
                               atlas_name, fill_direction_up )
@@ -131,7 +168,7 @@ subroutine field_initialiser( self, atlas_data_ptr, map_horizontal_ptr, &
     self%atlas_name = atlas_name
   else
     self%atlas_name = lfric_field_ptr%get_name()
-  endif
+  end if
 
   if ( present(surface_level_type) ) then
     ! Ensure the supplied option is valid
@@ -141,17 +178,17 @@ subroutine field_initialiser( self, atlas_data_ptr, map_horizontal_ptr, &
       write(log_scratch_space, '(A)') &
         "The supplied optional argument surface_level_type is not valid."
       call log_event( log_scratch_space, LOG_LEVEL_ERROR )
-    endif
+    end if
     self%surface_level_type = surface_level_type
   else
     self%surface_level_type = surface_level_present
-  endif
+  end if
 
   if (present(fill_direction_up)) then
     fill_direction_up_local = fill_direction_up
   else
     fill_direction_up_local = .true.
-  endif
+  end if
 
   ! Setup data sizes and indices
   self%n_horizontal = size(atlas_data_ptr,dim=2)
@@ -165,7 +202,7 @@ subroutine field_initialiser( self, atlas_data_ptr, map_horizontal_ptr, &
   else
     self%lfric_kstart = 1
     self%n_vertical_lfric = self%n_vertical
-  endif
+  end if
 
   if ( fill_direction_up_local ) then
     self%atlas_kstart = 1
@@ -175,13 +212,13 @@ subroutine field_initialiser( self, atlas_data_ptr, map_horizontal_ptr, &
     self%atlas_kstart = self%n_vertical
     self%atlas_kend = 1
     self%atlas_kdirection = -1
-  endif
+  end if
 
   ! Check inputs are consistent
 
   ! 0. Check input field is supported and get associated sizes
 
-  ! get the number of points in the LFRic field
+  ! Get the number of points in the LFRic field
   fs_enumerator = lfric_field_ptr%which_function_space()
   field_proxy=lfric_field_ptr%get_proxy()
   select case ( fs_enumerator )
@@ -203,7 +240,7 @@ subroutine field_initialiser( self, atlas_data_ptr, map_horizontal_ptr, &
       "Field mismatch in atlas field interface constructor for the number of vertical points. The atlas field has ", &
       self%n_vertical_lfric, " points and the LFRic field has ", n_vertical_lfric, " points."
     call log_event( log_scratch_space, LOG_LEVEL_ERROR )
-  endif
+  end if
 
   ! 2. Horizotal points
   if ( n_horizontal_lfric /= self%n_horizontal ) then
@@ -211,7 +248,7 @@ subroutine field_initialiser( self, atlas_data_ptr, map_horizontal_ptr, &
       "Field mismatch in atlas field interface constructor for the number of horizontal points. The atlas field has ", &
       self%n_horizontal, " points and the LFRic field has ", n_horizontal_lfric, " points."
     call log_event( log_scratch_space, LOG_LEVEL_ERROR )
-  endif
+  end if
 
   ! 3. Check the input horizontal map
 
@@ -222,14 +259,14 @@ subroutine field_initialiser( self, atlas_data_ptr, map_horizontal_ptr, &
       self%n_horizontal , " map_horizontal size = ", &
       size(self%map_horizontal)
     call log_event( log_scratch_space, LOG_LEVEL_ERROR )
-  endif
+  end if
 
   ! Check the input contains only unique points and that they are
   ! within the bounds of the field size
   allocate(check_map(self%n_horizontal))
   check_map = .false.
   do ij=1,self%n_horizontal
-    ! bounds check on input
+    ! Bounds check on input
     if ( self%map_horizontal(ij) > 0 .and. &
           self%map_horizontal(ij) <= self%n_horizontal ) then
       if ( check_map(self%map_horizontal(ij)) ) then
@@ -251,19 +288,23 @@ subroutine field_initialiser( self, atlas_data_ptr, map_horizontal_ptr, &
 
 end subroutine field_initialiser
 
-!> Copy Atlas field from the LFRic field
+!> @brief Copy Atlas field from the LFRic field
+!>
+!> @param [out] return_code Optional code that is allways set to zero if
+!>                          present
 subroutine copy_from_lfric(self, return_code)
 
   implicit none
 
   class( atlas_field_interface_type ), intent(inout) :: self
-  integer(i_def), intent(out), optional :: return_code
+  integer(i_def),              optional, intent(out) :: return_code
 
   type( field_type ), pointer :: lfric_field_ptr
   type( field_proxy_type )    :: field_proxy
   integer(i_def)              :: lfric_kstart
   integer(i_def)              :: lfric_ij
   integer(i_def)              :: atlas_ij
+  integer(i_def)              :: ij
   integer(i_def)              :: atlas_kstart
   integer(i_def)              :: atlas_kend
   integer(i_def)              :: atlas_kdirection
@@ -272,7 +313,7 @@ subroutine copy_from_lfric(self, return_code)
   lfric_field_ptr => self%get_lfric_field_ptr()
   field_proxy = lfric_field_ptr%get_proxy()
 
-  ! get indices for atlas and LFRic data
+  ! Get indices for atlas and LFRic data
   atlas_kstart = self % atlas_kstart
   atlas_kend = self % atlas_kend
   atlas_kdirection = self % atlas_kdirection
@@ -280,16 +321,23 @@ subroutine copy_from_lfric(self, return_code)
   lfric_kstart = self%lfric_kstart
   n_vertical_lfric = self%n_vertical_lfric
 
-  ! copy LFRic to Atlas
-  do lfric_ij = 1,self%n_horizontal
-    atlas_ij = self%map_horizontal(lfric_ij)
+  ! Copy LFRic to Atlas
+  do ij = 1,self%n_horizontal
+    ! Get LFRic and Atlas index
+    lfric_ij = (ij-1)*n_vertical_lfric
+    atlas_ij = self%map_horizontal(ij)
     self%atlas_data(atlas_kstart:atlas_kend:atlas_kdirection,atlas_ij) &
-      = field_proxy%data((lfric_ij-1)*n_vertical_lfric + lfric_kstart : lfric_ij*n_vertical_lfric)
-  enddo
+      = field_proxy%data(lfric_ij+lfric_kstart:lfric_ij+n_vertical_lfric)
+  end do
+
+  if ( present(return_code) ) return_code = 0_i_def
 
 end subroutine copy_from_lfric
 
-!> Copy Atlas field to the LFRic field
+!> @brief Copy Atlas field to the LFRic field
+!>
+!> @param [out] return_code Optional code that is allways set to zero if
+!>                          present
 subroutine copy_to_lfric( self, return_code )
 
   implicit none
@@ -300,6 +348,7 @@ subroutine copy_to_lfric( self, return_code )
   type( field_type ), pointer :: lfric_field_ptr
   type( field_proxy_type )    :: field_proxy
   integer(i_def)              :: lfric_kstart
+  integer(i_def)              :: ij
   integer(i_def)              :: lfric_ij
   integer(i_def)              :: atlas_ij
   integer(i_def)              :: atlas_kstart
@@ -310,7 +359,7 @@ subroutine copy_to_lfric( self, return_code )
   lfric_field_ptr => self%get_lfric_field_ptr()
   field_proxy = lfric_field_ptr%get_proxy()
 
-  ! get indices for atlas and LFRic data
+  ! Get indices for atlas and LFRic data
   lfric_kstart = self%lfric_kstart
   n_vertical_lfric = self%n_vertical_lfric
 
@@ -318,35 +367,192 @@ subroutine copy_to_lfric( self, return_code )
   atlas_kend = self % atlas_kend
   atlas_kdirection = self % atlas_kdirection
 
-  ! copy Atlas to lfric
+  ! Copy Atlas to lfric
 
   ! Fill data ommiting the lowest level if its not available
-  do lfric_ij = 1,self%n_horizontal
-    atlas_ij = self%map_horizontal(lfric_ij)
-    field_proxy%data((lfric_ij-1)*n_vertical_lfric+lfric_kstart:lfric_ij*n_vertical_lfric) &
+  do ij = 1,self%n_horizontal
+    atlas_ij = self%map_horizontal(ij)
+    lfric_ij = (ij-1)*n_vertical_lfric
+    field_proxy%data(lfric_ij+lfric_kstart:lfric_ij+n_vertical_lfric) &
       = self%atlas_data(atlas_kstart:atlas_kend:atlas_kdirection,atlas_ij)
-  enddo
+  end do
 
   ! Fill missing data if required
   if ( self%surface_level_type == surface_level_absent_zero_level ) then
     ! Set to zero
-    do lfric_ij = 1,self%n_horizontal
-      field_proxy%data((lfric_ij-1)*n_vertical_lfric+1) = 0.0!!! set _rdef?
+    do ij = 1,self%n_horizontal
+      lfric_ij = (ij-1)*n_vertical_lfric
+      field_proxy%data(lfric_ij+1) = 0.0_real64
     end do
   elseif ( self%surface_level_type == surface_level_absent_copy_level_above ) then
     ! Copy the level above
-    do lfric_ij = 1,self%n_horizontal
-      field_proxy%data((lfric_ij-1)*n_vertical_lfric+1) = &
-                      field_proxy%data((lfric_ij-1)*n_vertical_lfric+2)
+    do ij = 1,self%n_horizontal
+      atlas_ij = self%map_horizontal(ij)
+      lfric_ij = (ij-1)*n_vertical_lfric
+      field_proxy%data(lfric_ij+1) &
+          = self%atlas_data(atlas_kstart,atlas_ij)
     end do
-  endif
+  end if
 
-  ! set halo to dirty
+  ! Set halo to dirty
   call field_proxy%set_dirty()
+
+  if ( present(return_code) ) return_code = 0_i_def
 
 end subroutine copy_to_lfric
 
-!> Returns the lfric name of the field
+!> @brief   Adjoint of copy_from_lfric
+!>
+!> @detail  This is a line by line adjoint of the copy_from_lfric method
+!>          and as noted in the module detail results in data copies to the
+!>          LFRic field and initialization of the Atlas field to zero.
+subroutine copy_from_lfric_ad(self)
+
+  implicit none
+
+  class( atlas_field_interface_type ), intent(inout) :: self
+
+  type( field_type ), pointer :: lfric_field_ptr
+  type( field_proxy_type )    :: field_proxy
+  integer(i_def)              :: lfric_kstart
+  integer(i_def)              :: ij
+  integer(i_def)              :: lfric_ij
+  integer(i_def)              :: atlas_ij
+  integer(i_def)              :: atlas_kstart
+  integer(i_def)              :: atlas_kend
+  integer(i_def)              :: atlas_kdirection
+  integer(i_def)              :: n_vertical_lfric
+
+  lfric_field_ptr => self%get_lfric_field_ptr()
+  field_proxy = lfric_field_ptr%get_proxy()
+
+  ! Get indices for atlas and LFRic data
+  atlas_kstart = self % atlas_kstart
+  atlas_kend = self % atlas_kend
+  atlas_kdirection = self % atlas_kdirection
+
+  lfric_kstart = self%lfric_kstart
+  n_vertical_lfric = self%n_vertical_lfric
+
+  ! Adjoint of copy LFRic to Atlas
+  do ij = 1,self%n_horizontal
+    atlas_ij = self%map_horizontal(ij)
+    lfric_ij = (ij-1)*n_vertical_lfric
+    field_proxy%data(lfric_ij+lfric_kstart:lfric_ij+n_vertical_lfric) &
+        = field_proxy%data(lfric_ij+lfric_kstart:lfric_ij+n_vertical_lfric) &
+        + self%atlas_data(atlas_kstart:atlas_kend:atlas_kdirection,atlas_ij)
+  end do
+
+  ! Initialise out of scope variable to zero
+  self%atlas_data(:,:) = 0.0_real64
+
+end subroutine copy_from_lfric_ad
+
+!> @brief   Adjoint of copy_to_lfric
+!>
+!> @detail  This is a line by line adjoint of the copy_to_lfric method
+!>          and as noted in the module detail results in data copies to the
+!>          Atlas field and initialization of the LFRic field to zero.
+!>
+subroutine copy_to_lfric_ad( self )
+
+  implicit none
+
+  class( atlas_field_interface_type ), intent(inout) :: self
+
+  type( field_type ), pointer :: lfric_field_ptr
+  type( field_proxy_type )    :: field_proxy
+  integer(i_def)              :: lfric_kstart
+  integer(i_def)              :: ij
+  integer(i_def)              :: lfric_ij
+  integer(i_def)              :: atlas_ij
+  integer(i_def)              :: atlas_kstart
+  integer(i_def)              :: atlas_kend
+  integer(i_def)              :: atlas_kdirection
+  integer(i_def)              :: n_vertical_lfric
+
+  lfric_field_ptr => self%get_lfric_field_ptr()
+  field_proxy = lfric_field_ptr%get_proxy()
+
+  ! Get indices for atlas and LFRic data
+  lfric_kstart = self%lfric_kstart
+  n_vertical_lfric = self%n_vertical_lfric
+
+  atlas_kstart = self % atlas_kstart
+  atlas_kend = self % atlas_kend
+  atlas_kdirection = self % atlas_kdirection
+
+  ! Adjoint of (copy Atlas to lfric)
+
+  ! Adjoint of (fill missing data if required)
+
+  ! Note: The adjoint of (set to zero) is omitted for:
+  !       self%surface_level_type == surface_level_absent_zero_level. That is
+  !       because the operation is redundant, in this case the adjoint is:
+  !       self%atlas_data(:) = self%atlas_data(:) + 0.0.
+  !
+  if ( self%surface_level_type == surface_level_absent_copy_level_above ) then
+    ! Adjoint of (copy the level above)
+    do ij = 1,self%n_horizontal
+      atlas_ij = self%map_horizontal(ij)
+      lfric_ij = (ij-1)*n_vertical_lfric
+      self%atlas_data(atlas_kstart,atlas_ij) &
+                    = self%atlas_data(atlas_kstart,atlas_ij) &
+                    + field_proxy%data(lfric_ij+1)
+    end do
+  end if
+
+  ! Adjoint of (fill data ommiting the lowest level if its not available)
+  do ij = 1,self%n_horizontal
+    atlas_ij = self%map_horizontal(ij)
+    lfric_ij = (ij-1)*n_vertical_lfric
+    self%atlas_data(atlas_kstart:atlas_kend:atlas_kdirection,atlas_ij) &
+        = self%atlas_data(atlas_kstart:atlas_kend:atlas_kdirection,atlas_ij) &
+        + field_proxy%data(lfric_ij+lfric_kstart:lfric_ij+n_vertical_lfric)
+  end do
+
+  ! Initialise out of scope variable to zero
+  field_proxy%data(1:self%n_vertical_lfric*self%n_horizontal) = 0.0_real64
+
+  ! Set halo to dirty
+  call field_proxy%set_dirty()
+
+end subroutine copy_to_lfric_ad
+
+!> @brief Zero the LFRic field
+!>
+subroutine zero_lfric( self )
+
+  implicit none
+
+  class( atlas_field_interface_type ), intent(inout) :: self
+
+  type( field_type ), pointer :: lfric_field_ptr
+  type( field_proxy_type )    :: field_proxy
+
+  lfric_field_ptr => self%get_lfric_field_ptr()
+  field_proxy = lfric_field_ptr%get_proxy()
+
+  ! Set all data to zero
+  field_proxy%data(:) = 0.0_real64
+
+end subroutine zero_lfric
+
+!> @brief Zero the Atlas field
+!>
+subroutine zero_atlas( self )
+
+  implicit none
+
+  class( atlas_field_interface_type ), intent(inout) :: self
+
+  ! Set owned data to zero
+  self%atlas_data(:,:) = 0.0_real64
+
+end subroutine zero_atlas
+
+!> @brief Returns the lfric name of the field
+!>
 function get_lfric_name( self ) result( lfric_name )
 
   implicit none
@@ -360,7 +566,8 @@ function get_lfric_name( self ) result( lfric_name )
 
 end function get_lfric_name
 
-!> Returns the atlas name of the field
+!> @brief Returns the atlas name of the field
+!>
 function get_atlas_name( self ) result( atlas_name )
 
   implicit none
@@ -372,7 +579,8 @@ function get_atlas_name( self ) result( atlas_name )
 
 end function get_atlas_name
 
-!> Finalizer
+!> @brief atlas_field_interface_type finalizer
+!>
 subroutine atlas_field_interface_destructor(self)!
 
   implicit none
